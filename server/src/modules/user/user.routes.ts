50623d381b11c0138c4f2ab963a113b81df4407a
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2026-06-24 19:13:21 +0800

    refactor(user): add middleware

--- server/src/modules/user/user.routes.ts
+++ server/src/modules/user/user.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express';
 import AuthController from './auth.controllers';
+import { UserMiddleware } from './user.middleware';
 
 
 const userRouter = Router();
 
 userRouter.post('/signup', AuthController.signup);
 userRouter.post('/signin', AuthController.signin);
-userRouter.post('/logout', AuthController.logout);
+
+userRouter.post(
+  '/logout', 
+  UserMiddleware.attachUser,
+  AuthController.logout
+);
+
 userRouter.post('/reset-password', AuthController.resetPassword);
 userRouter.post('/forgot-password', AuthController.requestForgotPassword);
 
