0cd225341c71079993150e855711216abfc71c2f
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2026-06-25 00:58:39 +0800

    fix(branch): check record's uniqueness

--- server/src/modules/branch/branch.services.ts
+++ server/src/modules/branch/branch.services.ts
@@ -7,20 +7,26 @@ export class BranchServices {
     branchID: string,
     orgMemID: string,
     role: string,
-    inviteID: string
+    inviteID: string,
+    ...selectFields: string[]
   ) {
+    const slctStr = selectFields.join(", ");
+
     const { data, error } = await supabase
       .from('branch_members')
-      .insert({
+      .upsert({
         'branch_id': branchID,
         'org_mem_id': orgMemID,
         'role': role,
         'status': 'invited',
         'invitation_id': inviteID
+      }, {
+        onConflict: 'org_mem_id,branch_id',
+        ignoreDuplicates: true
       })
-      .select('id');
+      .select(slctStr);
 
-    if (!error) return data[0]?.id;
+    if (!error) return data[0];
 
     throw new InvalidCredentials(
       'Failed to store branch membership.'
