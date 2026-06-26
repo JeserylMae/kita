93211d93dddb7939b2a23435b4393ce0d1de8319
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2026-06-26 17:27:55 +0800

    feat(branch): add find branch memberships method

--- server/src/modules/branch/branch.services.ts
+++ server/src/modules/branch/branch.services.ts
@@ -1,5 +1,8 @@
 import { supabase } from "@/config/db";
-import { InvalidCredentials } from "@/errors";
+import { ErrorII, InvalidCredentials } from "@/errors";
+import { BrcMemberParams } from "./branch.types";
+import { BaseRepository } from "../base/base.repository";
+import { TableName } from "../organization/organization.types";
 
 
 export class BranchServices {
@@ -40,5 +43,51 @@ export class BranchServices {
     throw new InvalidCredentials(
       'Failed to store branch membership.'
     );
+  }
+
+  public static async findMembership(
+    id: string,
+    column: 'id'|'branch_id'|'org_mem_id'|'invitation_id' = 'id',
+    single = true
+  ) {
+    let builder = supabase
+      .from(TableName.branchMem)
+      .select(`
+        *,
+        roles(role),
+        branches(
+          branch_name,
+          organizations(org_name)
+        ),
+        organization_invitations(
+          *,
+          sender:users!organization_invitations_sender_id_fkey(
+            email
+          ),
+          receiver:users!organization_invitations_receiver_id_fkey(
+            firstname,
+            email
+          )
+        )
+      `)
+      .eq(column, id);
+
+    const { data, error } = single
+      ? await builder.single()
+      : await builder;
+
+    if (!error) return data;
+
+    throw new ErrorII(error.message);
+  }
+
+  public static async update<T extends Record<string, any>>(
+    branch: T,
+    table: TableName
+  ) {
+    const brcDB = new BaseRepository(table);
+    const data = await brcDB.upsert(branch);
+    
+    return data;
   }
 }
---
+++ server/src/modules/branch/branch.types.ts
@@ -0,0 +1,12 @@
+
+
+export interface BrcMemberParams {
+  id?: string;
+  branch_id?: string;
+  org_mem_id?: string;
+  role?: string;
+  status?: string;
+  starred?: boolean;
+  invitation_id?: string;
+  updated_at?: Date;
+}
