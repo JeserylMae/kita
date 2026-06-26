af739d73ab0fae250b99433f8d28d235a97eb4f7
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2026-06-27 01:12:55 +0800

    feat(org): implement additional controllers for org info and members
    methods: getMembers, create, update, updateMember, deleteMember

--- server/src/modules/organization/organization.controller.ts
+++ server/src/modules/organization/organization.controller.ts
@@ -1,6 +1,6 @@
 import { InvalidCredentials } from "@/errors";
 import { OrganizationService } from "./organization.services";
-import { OrgParams, TableName } from "./organization.types";
+import { OrgMembershipParams, OrgParams, TableName } from "./organization.types";
 import e, { NextFunction, Request, Response } from "express";
 import { MembershipServices } from "./membership.services";
 
@@ -39,15 +39,96 @@ export class OrganizationController {
     }
   }
 
-  public static create(req: Request, res: Response, next: NextFunction) {
+  /**
+   * 
+   * @param req 
+   * @param res 
+   * @param next 
+   */
+  public static async getMembers(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const orgID = req.params.orgID;
+
+      if (typeof orgID !== 'string') {
+        throw new InvalidCredentials(
+          'Organization ID must be string.'
+        );
+      }
+
+      const data = await MembershipServices
+        .findAllMembers(orgID);
+      
+      res.status(200).json({
+        "success": true,
+        "message": "Organization members are retrieved.",
+        "orgMembers": data
+      });
+    }
+    catch (error: unknown) {
+      next(error);
+    }
+  }
+
+  /**
+   * 
+   * @param req 
+   * @param res 
+   * @param next 
+   * @returns 
+   */
+  public static create(
+    req: Request, 
+    res: Response, 
+    next: NextFunction
+  ) {
     return this.save(req, res, next, 'create');
   }
 
-  public static update(req: Request, res: Response, next: NextFunction) {
+  /**
+   * 
+   * @param req 
+   * @param res 
+   * @param next 
+   * @returns 
+   */
+  public static update(
+    req: Request, 
+    res: Response, 
+    next: NextFunction
+  ) {
     return this.save(req, res, next, 'update');
   }
 
+  /**
+   * 
+   * @param req 
+   * @param res 
+   * @param next 
+   */
+  public static async updateMember(
+    req: Request<any, any, OrgMembershipParams>,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const member = req.body;
   
+      await MembershipServices.update(member);
+  
+      res.status(201).json({
+        'success': true,
+        'message': 'Member information was updated.'
+      });
+    }
+    catch(error: unknown) {
+      next(error);
+    }
+  }
+
   /**
    * Returns delete handler
    */
@@ -95,6 +176,33 @@ export class OrganizationController {
       next(error);
     }
   }
+
+  /**
+   * 
+   * @param req 
+   * @param res 
+   * @param next 
+   */
+  public static async deleteMember(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
+    try {
+      const memberID = req.params.id;
+
+      await MembershipServices.delete(memberID);
+      
+      res.status(200).json({
+        'success': true,
+        'message': `Member with ID ${memberID} was deleted.`
+      });
+    }
+    catch (error: unknown) {
+      next(error);
+    }
+  }
+
   /**
    * 
    * @param req 
