bf425ebfc6d2d29e3a777cc35a45dd67d77c5ff4
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2026-06-26 17:31:33 +0800

    feat(org): register organition routes

--- server/src/modules/organization/organization.routes.ts
+++ server/src/modules/organization/organization.routes.ts
@@ -5,11 +5,25 @@ import { OrganizationController } from "./organization.controller";
 
 const organizationRouter = Router();
 
-organizationRouter.get(
-  '/',
+organizationRouter.get('/',
   UserMiddleware.attachUser,
   OrganizationController.getOrganizations
 );
 
+organizationRouter.post('/',
+  OrganizationController.createOrganization
+);
+
+organizationRouter.delete('/founder/:id',
+  OrganizationController.deleteFounder
+);
+
+organizationRouter.delete('/brand/:id',
+  OrganizationController.deleteBrand
+);
+
+organizationRouter.delete('/:id',
+  OrganizationController.deleteOrg
+);
 
 export default organizationRouter;
