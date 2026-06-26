833ee47892939cd7ad3f6a50c35522581192b90f
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2026-06-27 01:11:17 +0800

    feat(org-membership): implement find members and delete

--- server/src/modules/organization/membership.services.ts
+++ server/src/modules/organization/membership.services.ts
@@ -1,71 +1,104 @@
 import { supabase } from "@/config/db";
 import { BaseRepository } from "../base/base.repository";
-import { OrgMembershipParams } from "./organization.types";
-import { InvalidCredentials, RecordNotFound } from "@/errors";
+import { OrgMembershipParams, TableName } from "./organization.types";
+import { ErrorII, InvalidCredentials, RecordNotFound } from "@/errors";
+import { sanitizeObject } from "@/utils/data.helpers";
 
 
 export class MembershipServices {
-    /**
-     * 
-     * @param userID 
-     * @param options 
-     * @returns 
-     */
-    public static async findMembership(
-      userID: string,
-      options?: {
-        withBranches?: boolean,
-        defaultOrgOnly?: boolean
-      }
-    ) {
-      // organization_members.org_id = organizations.id
-      let slctStr = (`
-        org_id,
-        is_default_org,
-        employee_code,
-        status,
-        organizations( org_name, icon, hex_color )
-      `);
-  
-      if (options?.withBranches) {
-        // organization_members.id = branch_members.org_mem_id
-        // branch_members.branch_id = branches-id
-        slctStr += (`
-          ,
-          branch_members (
-            branch_id,
-            role(role),
-            status,
-            starred,
-            branches( branch_name, icon, color )
-          )
-        `);
-      }
-
-      let buidler = supabase 
-        .from('organization_members')
-        .select(slctStr)
-        .eq('user_id', userID)
-        .eq('organizations.status', 'active')
-        .eq('status', 'active');
-    
-      buidler = options?.withBranches
-        ? buidler.eq('branches.status', 'active')
-        : buidler;
-      
-      buidler = options?.defaultOrgOnly 
-        ? buidler.eq('is_default_org', true)
-        : buidler;
-  
-      const { data, error } = await buidler;
-      
-      if (!error) return data;
-      
-      throw new RecordNotFound(
-        'Failed to fetch organization list.'
-      );
+  /**
+   * 
+   * @param userID 
+   * @param options 
+   * @returns 
+   */
+  public static async findMembership(
+    userID: string,
+    options?: {
+      withBranches?: boolean,
+      defaultOrgOnly?: boolean
     }
- 
+  ) {
+    // organization_members.org_id = organizations.id
+    let slctStr = (`
+      org_id,
+      is_default_org,
+      employee_code,
+      status,
+      organizations( org_name, icon, hex_color )
+    `);
+
+    if (options?.withBranches) {
+      // organization_members.id = branch_members.org_mem_id
+      // branch_members.branch_id = branches-id
+      slctStr += (`
+        ,
+        branch_members (
+          branch_id,
+          role(role),
+          status,
+          starred,
+          branches( branch_name, icon, color )
+        )
+      `);
+    }
+
+    let buidler = supabase 
+      .from('organization_members')
+      .select(slctStr)
+      .eq('user_id', userID)
+      .eq('organizations.status', 'active')
+      .eq('status', 'active');
+  
+    buidler = options?.withBranches
+      ? buidler.eq('branches.status', 'active')
+      : buidler;
+    
+    buidler = options?.defaultOrgOnly 
+      ? buidler.eq('is_default_org', true)
+      : buidler;
+
+    const { data, error } = await buidler;
+    
+    if (!error) return data;
+    
+    throw new RecordNotFound(
+      'Failed to fetch organization list.'
+    );
+  }
+
+  public static async findAllMembers( orgID: string ) {
+    const { data, error } = await supabase
+      .from(TableName.orgMem)
+      .select(`
+        id,
+        user_id,
+        users(
+          firstname,
+          middlename,
+          lastname, 
+          suffix,
+          house_number,
+          street,
+          barangay,
+          city,
+          province,
+          region,
+          birthdate,
+          email
+        ),
+        status,
+        employee_code,
+        employment_date,
+        updated_at
+      `)
+      .eq('org_id', orgID);
+    
+    if (!error) return data;
+
+    throw new ErrorII(error.message);
+  }
+
   /**
    * 
    * @param orgID 
@@ -119,46 +152,25 @@ export class MembershipServices {
   public static async update( 
     org: OrgMembershipParams 
   ) {
-    const orgDB = new BaseRepository("organization_members");
-    const data = await orgDB.upsert(org);
+    const orgDB = new BaseRepository(TableName.orgMem);
+
+    org.updated_at = new Date();
+  
+    const odata = sanitizeObject(org);
+    const data = await orgDB.upsert(odata);
 
     return data;
   }
+
+  public static async delete( memberID: any ) {
+    if (typeof memberID !== 'string') {
+      throw new InvalidCredentials(
+        'Member ID must be a string.'
+      );
+    }
+
+    const orgDB = new BaseRepository(TableName.orgMem);
+
+    await orgDB.delete(memberID);
+  }
 }
-
-
-/**
- * CREATE INVITATIONS
- *  - create a record on:
- *      - org_mem
- *      - brc_mem
- * CREATE ORGANIZATIONS
- *  - create a record on:
- *      - org_mem
- */
-
-/**
- * FETCH ORG EMPLOYEES
- * - from organizations
- * - join through org_mem(org_id)
- * 
- * FETCH USER'S ORGS 
- * - from org_mem
- * - join organizations(id)
- * 
- * FETCH USER'S ORGS + BRCS
- * - from org_mem
- * - select(
- *  *,
- *  organizations(
- *    *,
- *    branches (
- *      *,
- *      branch_mem(
- *        *
- *      )
- *    )
- *  )
- * )
- * 
- */
