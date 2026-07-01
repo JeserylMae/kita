baa2d6a621dcd27ff66bb45161b7176ee4bcfd95
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2026-07-01 16:15:14 +0800

    feat(items): implement controllers and services for inventory item requests

---
+++ server/src/modules/inventory/items/items.types.ts
@@ -0,0 +1,31 @@
+
+
+export interface ItemInsert {
+  product_variant_id: string;
+  branch_id: string;
+  item_code: string;
+  batch_code: string;
+  init_quantity: number;
+  reorder_level: number;
+  unit_cost: number   
+  unit_price: number;
+  expiry_date: Date;
+  remarks?: string;
+  created_by?: string;
+}
+
+export interface ItemUpdate {
+  id: string;
+  branch_id: string;
+  product_variant_id?: string;
+  item_code?: string;
+  batch_code?: string;
+  init_quantity?: number;
+  current_quantity?: number;
+  reorder_level?: number;
+  unit_cost?: number   
+  unit_price?: number;
+  expiry_date?: Date;
+  status?: 'in stock' | 'low stock' | 'out of stock' | 'expired';
+  remarks?: string;
+}
