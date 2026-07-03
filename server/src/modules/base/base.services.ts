cbdff9c61465b9edbcb5d6e790e2f5ef94440599
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2026-07-03 14:56:30 +0800

    feat(base): implement get enum array from db

--- server/src/modules/base/base.services.ts
+++ server/src/modules/base/base.services.ts
@@ -1,4 +1,6 @@
-import { InvalidCredentials } from "@/errors";
+import { ErrorII, InvalidCredentials } from "@/errors";
+import { EnumName } from "./base.types";
+import { supabase } from "@/config/db";
 
 
 export const canAccessUser = ( scope: string[] | undefined ) => {
@@ -21,4 +23,15 @@ export const canAccessUser = ( scope: string[] | undefined ) => {
   }
 
   throw new InvalidCredentials(errMsg);
+}
+
+export const getEnum = async ( name: EnumName ) => {
+  const { data, error } = await supabase
+    .rpc('get_enum_values', {
+      'enum_name': name
+    });
+  
+  if (!error) return data;
+
+  throw new ErrorII(error.message);
 }
