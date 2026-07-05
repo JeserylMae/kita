5264d606deb0947386e41590db409aea5a2edbbc
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2026-07-05 16:15:17 +0800

    feat(transaction): specify request parameters

---
+++ server/src/modules/inventory/transaction/transaction.types.ts
@@ -0,0 +1,122 @@
+
+export type ReferenceTypeKeys = keyof typeof ReferenceType;
+
+export interface TransactionInsert {
+  branch_id: string;
+  amount: number;
+  payment_method: string;
+  reference_type: string;
+  reference_id: string;
+  code: string;
+  created_by_name?: string;
+  created_by_role?: string;
+}
+
+
+export interface TransactionUpdate {
+  amount?: number;
+  payment_method?: string;
+  reference_type?: string;
+  reference_id?: string;
+  code?: string;
+  created_by_name?: string;
+  created_by_role?: string;
+}
+
+export const ReferenceType = {
+  'sales invoice': {
+    table: 'sales_invoice_items',
+    id: 'sales_invoice_id',
+    select: (`
+      id,
+      product_variants(id, sku),
+      quantity,
+      unit_price,
+      unit_cost,
+      line_total
+    `)
+  }, 
+  'purchase invoice': {
+    table: 'purchase_invoice_items',
+    id: 'purchase_invoice_id',
+    select: (`
+      id,
+      product_variants(id, sku),
+      quantity,
+      unit_price,
+      unit_cost,
+      line_total
+    `)
+  }, 
+  'refund': {
+    table: 'refund_items',
+    id: 'refund_id',
+    select: (`
+      id,
+      sales_invoice_items(
+        id, 
+        product_variants(id, sku),
+        unit_price,
+        unit_cost,
+      ),
+      quantity, 
+      amount
+    `)
+  },
+  'return': {
+    table: 'refund_items',
+    id: 'refund_id',
+    select: (`
+      id,
+      sales_invoice_items(
+        id, 
+        product_variants(id, sku),
+        unit_price,
+        unit_cost,
+      ),
+      quantity, 
+      amount
+    `)
+  },
+  'stock adjustment': {
+    table: 'inventory_movements',
+    id: 'id',
+    select: (`
+      id, 
+      product_variants(id, sku),
+      quantity_changed,
+      movement_type,
+      inventory_items(
+        batch_code,
+        unit_price,
+        unit_cost
+      )
+    `)
+  }, 
+  'stock transfer':{
+    table: 'inventory_movements',
+    id: 'id',
+    select: (`
+      id, 
+      product_variants(id, sku),
+      quantity_changed,
+      movement_type,
+      inventory_items(
+        batch_code,
+        unit_price,
+        unit_cost
+      )
+    `)
+  },  
+  'supplier payment':{
+    table: 'expenses',
+    id: 'id',
+    select: (`
+      id, 
+      categories(*), 
+      type, 
+      amount, 
+      payment_method
+    `)
+  },
+}
