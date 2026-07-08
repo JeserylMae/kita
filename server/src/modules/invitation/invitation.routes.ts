f4aa7fbe15be56103ac59a58bc704f4da7bdd95d
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2026-07-09 00:14:25 +0800

    fix(invitation): use route middleware

--- server/src/modules/invitation/invitation.routes.ts
+++ server/src/modules/invitation/invitation.routes.ts
@@ -1,31 +1,46 @@
 import { Router } from "express";
 import * as InvitationController from "./invitation.controller";
+import { requireAuth, requireBrc, requireOrg, verifyBrcPermission } from "@/middleware/auth.middleware";
 
 
 const invitationRouter = Router();
 
 // /invitation/
 invitationRouter.post('/', 
+  requireAuth,
+  requireOrg,
+  requireBrc,
+  verifyBrcPermission('insert.orginv'),
   InvitationController.invite
 );
 
 // /invitation/tokenstr
 invitationRouter.post('/:token', 
+  requireAuth,
   InvitationController.respondToInvitation
 );
 
 // /invitation/idstr
 invitationRouter.post('/:id',
+  requireAuth,
   InvitationController.reinvite
 );
 
 // /invitation/idstr
-invitationRouter.get('/:id',
+invitationRouter.get('/me',
+  requireAuth,
+  requireOrg,
+  requireBrc,
+  verifyBrcPermission('select.orginv'),
   InvitationController.getInvitations
 );
 
 // /invitation/idstr
 invitationRouter.delete('/:id',
+  requireAuth,
+  requireOrg,
+  requireBrc,
+  verifyBrcPermission('delete.orginv'),
   InvitationController.deleteInvitation
 );
 
