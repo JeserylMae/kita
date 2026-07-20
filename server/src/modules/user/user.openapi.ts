93d5f69da2ed37e5dd0611be17a82ae2870ae377
JeserylMae <128215077+JeserylMae@users.noreply.github.com>
2026-07-21 00:22:06 +0800

    fix(user): add header parameter

--- server/src/modules/user/user.openapi.ts
+++ server/src/modules/user/user.openapi.ts
@@ -66,6 +66,20 @@ export const RefreshPath: ZodOpenApiPathItemObject = {
     description: "Create new access token.",
     security:    [{ cookieAuth: [] }],
 
+    parameters: [
+      {
+        in: "header",
+        name: "Token-Action",
+        required: true,
+        schema: {
+          type: "string",
+          enum: ["refresh"],
+        },
+        description: "Indicates that this request is for refreshing the access token.",
+        example: "refresh",
+      },
+    ],
+
     responses: {
       200: { description: "Refresh successful." },
       400: { description: "Invalid reset token or request data" },
